5bf1242fdb45ca183f18b150bbda2809643931de
moss <sunruh@proton.me>
2026-05-25 12:29:49 +0200

    feat/add command bar

---
+++ src/app/command.rs
@@ -0,0 +1,24 @@
+#[derive(Default)]
+pub struct CommandBar {
+    pub text_field: String,
+    pub location_pointer: usize,
+}
+
+impl CommandBar {
+    pub fn append(&mut self, character: char) {
+        self.text_field.insert(self.location_pointer, character);
+        self.location_pointer += 1;
+    }
+
+    pub fn backspace(&mut self) {
+        if self.location_pointer == 0 {
+            return;
+        }
+        self.text_field.remove(self.location_pointer - 1);
+        self.location_pointer -= 1;
+    }
+    pub fn reset(&mut self) {
+        self.text_field.clear();
+        self.location_pointer = 0;
+    }
+}
