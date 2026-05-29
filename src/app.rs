7b96a56f0801549bfe8c9f6b5b4fa9acedf46ef7
moss <sunruh@proton.me>
2026-05-29 14:08:37 +0200

    feat/build scaffolding for event loop

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,22 @@
+use crossterm::event::KeyEvent;
+
 mod command;
 mod interpreter;
 mod parser;
 
+#[derive(Default)]
 pub struct AppState {
     pub should_quit: bool,
 }
 
+#[derive(Default)]
 pub struct App {
     pub app_state: AppState,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn update(&mut self, key: KeyEvent) {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 mod app;
+mod ui;
 
-fn main() {
-    println!("Hello, world!");
+use crossterm::event::Event;
+
+use crossterm::event;
+use std::io::Result;
+
+use crate::app::App;
+
+fn main() -> Result<()> {
+    let mut app = App::new();
+    let mut terminal = ratatui::init();
+
+    loop {
+        terminal.draw(|frame| ui::render(frame, &app))?;
+        match event::read()? {
+            Event::Key(key) => app.update(key),
+            _ => {}
+        }
+
+        if app.app_state.should_quit {
+            break;
+        }
+    }
+    ratatui::restore();
+    Ok(())
 }
