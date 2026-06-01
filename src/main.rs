d7a67727b59b1309e333e95112dd4980c959a08f
moss <sunruh@proton.me>
2026-06-01 13:03:45 +0200

    chore/add simplelog and log dependency

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ mod ui;
 use crossterm::event::Event;
 
 use crossterm::event;
+use log::{error, info, warn};
+use simplelog::{Config, WriteLogger};
+use std::fs::File;
 use std::io::Result;
 
 use crate::app::App;
@@ -11,6 +14,15 @@ use crate::app::App;
 fn main() -> Result<()> {
     let mut app = App::new();
     let mut terminal = ratatui::init();
+
+    let log_file = File::create("app.log").expect("Failed to create log file");
+
+    WriteLogger::init(log::LevelFilter::Info, Config::default(), log_file)
+        .expect("Failed to Initialize logger");
+
+    info!("The application has successfully started.");
+    warn!("This is a warning message.");
+    error!("Something went wrong!");
 
     loop {
         terminal.draw(|frame| ui::render(frame, &app))?;
